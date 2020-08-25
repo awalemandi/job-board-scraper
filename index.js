e95cbadc4758a370bb4b44e4cd5cb61d9a33833d
awalemandi <mandi.awale@gmail.com>
2020-08-25 15:09:14 +1000

    job scrabe / project-1 complete

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const { Sheet } = require('./sheet');
 const fetch = require('node-fetch');
 
-const jobSiteUrl = 'https://jobs.github.com/positions.json?description=ruby&page=1';
+// const jobSiteUrl = `https://jobs.github.com/positions.json?page=1&search=code`;
 const googleSheetId = '1CukE2LF0bZF8LAOPdh7kWGnIEYMbqEA88a1bvwYzI9M';
-
-(async function () {                         //Immediately Invoked Function Expression (IIFE)
+const searchCriteria = `code`;
+                                                     //Immediately Invoked Function Expression (IIFE)
+async function getJobs(i) {                        //i = results page number
    
-    const result = await fetch(jobSiteUrl);
+    const result = await fetch(`https://jobs.github.com/positions.json?page=${i}&search=${searchCriteria}`);
     const json = await result.json();
 
-    console.log({ json });                          //display the result array in json
+    // console.log({ json });                          //display the result array in json
     const rows = json.map(job => {                  //map json object to match google sheet columns
         return {
             company: job.company,
@@ -19,12 +20,27 @@ const googleSheetId = '1CukE2LF0bZF8LAOPdh7kWGnIEYMbqEA88a1bvwYzI9M';
             date: job.created_at,
             url: job.url
         }
-    });
+    })
     
     const jobSheet = new Sheet(googleSheetId);
     await jobSheet.load();
 
     await jobSheet.addRows(rows);
+    return rows;
+}
 
+//function to filter for roles using .includes()//sort by searchCriteria
+//function to sort() jobs by date before populating sheet
+
+(async function () {
+    let i = 1;
+    let rows = [];
+    while (true) {                      //loop to combine results until page with no results
+        const newRows = await getJobs(i);
+        if (newRows.length === 0) break;
+        rows = rows.concat(newRows);
+        // console.log(`current row length: ${newRows.length}`);
+        i++;
+    }
+    // console.log(`Total row length: ${rows.length}`);
 })();
-
